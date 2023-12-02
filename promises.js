223dcb3674a24551bd51d09bfdec7c8d1c7599ee
emrys57 <e.williams@linkz-im.com>
2023-12-02 16:11:08 +0000

    Convert to ES modules

--- promises.js
+++ promises.js
@@ -1,28 +1,14 @@
 // promises to access the ADS1x15
 
 // single-ended read
-const promiseToRead = async ({
-  adc,
-  channel,
-  progGainAmp,
-  samplesPerSecond,
-}) => {
+export const promiseToRead = async ({adc, channel, progGainAmp, samplesPerSecond}) => {
   return new Promise((resolve, reject) => {
-    adc.readADCSingleEnded(
-      channel,
-      progGainAmp,
-      samplesPerSecond,
-      (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
+    adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
       }
-    );
+    });
   });
-};
-
-module.exports = {
-  promiseToRead,
 };
--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-var ads1x15 = require('node-ads1x15');
-var chip = 1; //0 for ads1015, 1 for ads1115
-const {promiseToRead} = require('./promises');
+import ads1x15 from 'node-ads1x15';
+import {promiseToRead} from './promises.js';
+
+const chip = 1; //0 for ads1015, 1 for ads1115
 
 //Simple usage (default ADS address on pi 2b or 3):
 const adc = new ads1x15(chip);
 
 // Optionally i2c address as (chip, address) or (chip, address, i2c_dev)
 // So to use  /dev/i2c-0 use the line below instead...:
-
-//    var adc = new ads1x15(chip, 0x48, 'dev/i2c-0');
+//    const adc = new ads1x15(chip, 0x48, 'dev/i2c-0');
 
 const channel = 0; //channel 0, 1, 2, or 3...
 const samplesPerSecond = '250'; // see index.js for allowed values for your chip
@@ -21,4 +21,4 @@ const test1 = async () => {
   }
 };
 
-await test1();
+test1().catch(console.error);
